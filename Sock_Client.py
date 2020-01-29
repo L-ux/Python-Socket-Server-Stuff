c988920ff72eee0681dbfe9984ae2b0763feb323
Josh Grillo <Josh@Grillo.co.uk>
2020-01-29 10:52:39 +0000

    Basic Client/Server from slide deck implemented

---
+++ Sock_Client.py
@@ -0,0 +1,14 @@
+import socket
+
+if __name__ == '__main__':
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    mySocket.connect(("127.0.0.1", 8222))
+
+    testString = "WHAT FUCK"
+
+    mySocket.send(testString.encode())
+
+    while True:
+        data = mySocket.recv(4096)
+        print(data.decode("utf-8"))
